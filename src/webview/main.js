f7e88d90a5c9fe58073aba27d3fe3ea5b2be30dd
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-10-30 09:18:30 -0500

    created webview

---
+++ src/webview/main.js
@@ -0,0 +1,37 @@
+const vscode = acquireVsCodeApi();
+
+const languageDropdown = document.getElementById('languageSelect');
+const processBtn = document.getElementById('processBtn');
+const userInput = document.getElementById('userInput');
+const output = document.getElementById('output');
+const aiOutput = document.getElementById('aiOutput');
+
+languageDropdown.addEventListener('change', () => {
+    if (languageDropdown.value != 'languageSelect')
+    {
+        vscode.postMessage({
+        command: 'dropdownChange',
+        language: languageDropdown.value
+    });
+    }
+});
+
+processBtn.addEventListener('click', () => {
+    vscode.postMessage({
+        command: 'processCode',
+        userInput: userInput.value,
+        output: output.textContent
+    });
+});
+
+window.addEventListener('message', event => {
+    const message = event.data;
+
+    if (output && message.command === 'updateOutput') {
+        output.textContent = message.text;
+    }
+
+    if (aiOutput && message.command === 'updateAIOutput') {
+        aiOutput.textContent = message.text;
+    }
+});
